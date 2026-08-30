b44c2aef6e29de6341d9072ea8cf3f2a6546466c
Iftekhar Ahmed <iftekharifat007@gmail.com>
2026-08-30 21:43:27 +0300

    updated some writing

--- src/components/home/about-section.tsx
+++ src/components/home/about-section.tsx
@@ -3,7 +3,7 @@ import RemoteMDX from "../shared/remote-mdx";
 import TimelineMDXWrapper from "../shared/shared-component";
 
 export default function AboutSection() {
-  const about = `Hello 👋 I am Iftekhar, an aspiring AI researcher studying my **master's at University Of Helsinki in Data Science** and working with [CCDS](https://ccds.ai/) and [InteX](https://www.intexlab.net/). My research interests lie in **computer vision**. I am currently exploring **Vision-Language Models (VLMs)**, **Vision-Language-Action (VLA) systems**, and **Agentic AI**. I enjoy building tools and software—mostly things I find genuinely useful, but also ideas that simply interest me. I'm always open to research collaborations and exciting software projects.`;
+  const about = `Hello 👋 I am Iftekhar, an aspiring AI researcher studying my **master's at the University of Helsinki in Data Science** and working with [CCDS](https://ccds.ai/) and [InteX](https://www.intexlab.net/). My research interests lie in **computer vision**. I am currently exploring **Vision-Language Models (VLMs)**, **Vision-Language-Action (VLA) systems**, and **Agentic AI**. I enjoy building tools and software—mostly things I find genuinely useful, but also ideas that simply interest me. I'm always open to research collaborations and exciting software projects.`;
 
   return (
     <div className="font-mono max-w-full">
